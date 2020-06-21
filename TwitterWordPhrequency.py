b8dfe34e717f206d854eb3e78867ebb8e7507a80
SocialChengineer <chenb0xllc@gmail.com>
2020-06-21 12:43:17 -0400

    Separating RT posts. Start of data cleaning.

--- TwitterWordPhrequency.py
+++ TwitterWordPhrequency.py
@@ -4,6 +4,13 @@ import twitfunctions as twit
 import datetime as dt
 import json
 import sys
+import re
+import nltk
+
+def retweet_capture(tweet: str) -> dict:
+    """
+    Separates RT tweets into it's own dictionary
+    """
 
 if len(sys.argv) != 2:
     raise ValueError('Please enter a twitter handle to analyze')
@@ -12,15 +19,20 @@ if len(sys.argv) != 2:
 target = twit.getUserInfobyName(sys.argv[1])
 target_name = target['name']
 target_id = target['id']
-last_status = target['status']['text']
 
 # Grab the latest sample of the target timeline
 count = 150
 target_timeline = twit.getUserTimeline(sys.argv[1], count)
 
 word_dict = {}
+rt_list = []
+
 
 for id in target_timeline:
+    if id['text'].startswith('RT'):
+        rt_list.append(id['text'])
+        pass
+    
     curr_string = id['text'].split()
     for word in curr_string:
         if word not in word_dict:
@@ -31,5 +43,8 @@ for id in target_timeline:
             word_dict[word] += 1
             print(f'{word}: {word_dict[word]}')
 
-#print(f'{target_name} {target_id} {last_status}')
-#print(f'{target["status"]["id"]}')
+for rt in rt_list:
+    print(rt)
+
+# print(f'{target_name} {target_id} {last_status}')
+# print(f'{target["status"]["id"]}')
