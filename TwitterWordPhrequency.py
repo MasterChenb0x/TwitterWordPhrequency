7c7390e43f10aecb66bd2c129095a8883bdcb536
MasterChenb0x <artofmorellc@gmail.com>
2020-07-11 02:25:19 -0400

    file exceptiono handling

--- TwitterWordPhrequency.py
+++ TwitterWordPhrequency.py
@@ -6,35 +6,50 @@ import pandas as pd
 import json
 import sys
 import re
-import nltk
 import os
-from os import path
 
 
 if len(sys.argv) != 2:
     raise ValueError('Please enter a twitter handle to analyze')
 
-count = 200
-
-# Grab target information
+# Grab target information from file OR from Twitter if new
 target = sys.argv[1]
 try:
-    with open(f'{target}.json', 'r') as timeline_file:
+    with open(f'{target}.json', 'r') as target_file:
+        target_json = json.load(target_file)
+    target_file.close()
+    target_json = json.dumps(target_json, indent=4)
+except IOError:
+    target_dict = twit.getUserInfobyName(target)
+    target_json = json.dumps(target_dict, indent=4)
+    with open(f'{target}.json', 'w') as target_file:
+        json.dump(target_dict, target_file)
+    target_file.close()
+
+target_name = target_dict['name']
+target_id = target_dict['id']
+
+
+# Grab timeline from file OR Twitter if new
+count = 200
+try: 
+    with open(f'{target}_timeline.txt', 'r') as timeline_file:
         timeline_json = json.load(timeline_file)
     timeline_file.close()
     timeline_json = json.dumps(timeline_json, indent=4)
 except IOError:
-    # Grab the latest sample of the target timeline
-    target_dict = twit.getUserInfobyName(target)
-    target_name = target_dict['name']
-    target_id = target_dict['id']
-    target_timeline = twit.getUserTimeline(target, count)
+    target_timeline_dict = twit.getUserTimeline(target, count)
+    timeline_json = json.dumps(target_timeline_dict, indent=4)
+    with open(f'{target}_timeline.txt' 'w') as timeline_file:
+        json.dump(target_timeline_dict, timeline_file)
+    timeline_file.close()
+
 
 word_dict = {}
 rt_list = []
 hashtags = []
 
-for id in target_timeline:
+for id in target_timeline_dict:
     if id['text'].startswith('RT'):
         rt_list.append(id['text'])
         pass
